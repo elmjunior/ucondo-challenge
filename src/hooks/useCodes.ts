08c05ddc1ef2c53173622f1098e232191f055baf
Edilson Junior <elmjunior@gmail.com>
2021-05-25 08:56:39 -0300

    feat: add try catch to code generator

--- src/hooks/useCodes.ts
+++ src/hooks/useCodes.ts
@@ -6,31 +6,35 @@ export default function useCodes() {
   const canIncrement = (num: string | number) => Number(num) < 999;
 
   const getCode = async (item: RegisterItem) => {
-    const prefix = item.code;
-    const children = await getLastChild(item);
-    if (!children?.length) {
-      return `${prefix}.1`;
-    }
-
-    const code = children[0].code.split(".");
-
-    if (canIncrement(code[code.length - 1])) {
-      return code
-        .map((x, i) => (i === code.length - 1 ? Number(x) + 1 : x))
-        .join(".");
-    }
-
-    for (let i = code.length - 1; i >= 0; i -= 1) {
-      let previous;
-      if (i !== 0) previous = code[i - 1];
-      if (canIncrement(previous)) {
-        const filtered = code.filter((x, y) => y < i);
-        filtered[filtered.length - 1] = `${
-          Number(filtered[filtered.length - 1]) + 1
-        }`;
-
-        return filtered.join(".");
+    try {
+      const prefix = item.code;
+      const children = await getLastChild(item);
+      if (!children?.length) {
+        return `${prefix}.1`;
       }
+
+      const code = children[0].code.split(".");
+
+      if (canIncrement(code[code.length - 1])) {
+        return code
+          .map((x, i) => (i === code.length - 1 ? Number(x) + 1 : x))
+          .join(".");
+      }
+
+      for (let i = code.length - 1; i >= 0; i -= 1) {
+        let previous;
+        if (i !== 0) previous = code[i - 1];
+        if (canIncrement(previous)) {
+          const filtered = code.filter((x, y) => y < i);
+          filtered[filtered.length - 1] = `${
+            Number(filtered[filtered.length - 1]) + 1
+          }`;
+
+          return filtered.join(".");
+        }
+      }
+    } catch (error) {
+      return "";
     }
   };
   return { getCode };
